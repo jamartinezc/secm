0e959a11d017652a24144445dd7b85e910f92328
jaguar.scratch <jaguar.scratch@6511733c-373b-11de-b438-597f59cd7555>
2009-05-02 17:14:39 +0000

    Creacion de estructura de directorios y clases entidad (Por Angela)

---
+++ trunk/Negocio/src/negocio/ListaNoReceptores.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package negocio;
+
+/**
+ *
+ * @author Administrador
+ */
+public class ListaNoReceptores {
+    
+    private String[] noReceptor;
+
+    public String[] getNoReceptor() {
+        return noReceptor;
+    }
+
+    public void setNoReceptor(String[] noReceptor) {
+        this.noReceptor = noReceptor;
+    }
+
+}
