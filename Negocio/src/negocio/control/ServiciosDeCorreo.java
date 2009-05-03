3dd126b91d67a8b689a4358cf1bb8827fb3aa337
elijutisUN <elijutisUN@6511733c-373b-11de-b438-597f59cd7555>
2009-05-03 00:06:29 +0000

    Caso de uso 5 parcialmente completo

--- Negocio/src/negocio/control/ServiciosDeCorreo.java
+++ Negocio/src/negocio/control/ServiciosDeCorreo.java
@@ -10,5 +10,12 @@ package negocio.control;
  * @author JORGE
  */
 public class ServiciosDeCorreo {
+    
+    private ConfiguradoraServidorSMTP configuradorasServidorSMPT;
+    
+    public boolean GuardarSMTP(String host,int puerto, boolean usarSSL, String correoDestinatario, char[] contraseña){
+        configuradorasServidorSMPT.CrearServidorSMTP(host,puerto, usarSSL, correoDestinatario, contraseña);
+        return true;
+    }
 
 }
