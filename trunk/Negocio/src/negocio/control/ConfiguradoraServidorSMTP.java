84fa96fd4e7bda319ab3704fa9f6d405764433ef
elijutisUN <elijutisUN@6511733c-373b-11de-b438-597f59cd7555>
2009-05-03 00:06:29 +0000

    Caso de uso 5 parcialmente completo

--- trunk/Negocio/src/negocio/control/ConfiguradoraServidorSMTP.java
+++ trunk/Negocio/src/negocio/control/ConfiguradoraServidorSMTP.java
@@ -5,10 +5,26 @@
 
 package negocio.control;
 
+import java.util.LinkedList;
+import negocio.entidades.ServidorSMTP;
+
 /**
  *
  * @author JORGE
  */
 public class ConfiguradoraServidorSMTP {
+    
+    private LinkedList Servidores;
+    
+    public boolean CrearServidorSMTP(String host,int puerto, boolean usarSSL, String correoDestinatario, char[] contraseña){
+        ServidorSMTP nuevoServidor = new ServidorSMTP();
+        nuevoServidor.setContraseña(contraseña);
+        nuevoServidor.setCorreoDestinatario(correoDestinatario);
+        nuevoServidor.setHost(host);
+        nuevoServidor.setPuerto(puerto);
+        nuevoServidor.setUsarSSL(usarSSL);
+        Servidores.add(nuevoServidor);
+        return true;
+    }
 
 }
